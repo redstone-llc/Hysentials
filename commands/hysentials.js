b08106c3a84b27120b0af8f79d51d8b9a125210f
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2022-12-11 17:22:12 +0100

    Optimize

--- commands/hysentials.js
+++ commands/hysentials.js
@@ -1,5 +1,8 @@
 import Settings from '../utils/config';
 
+let cen1 = ChatLib.getCenteredText("&6Hysentials Commands (1/1)")
+let cem2 = ChatLib.getCenteredText('&7Basic Hysentials Commands')
+
 let hysentialsCommand = register("command", ...args => {
     let command;
     try {
@@ -12,8 +15,8 @@ let hysentialsCommand = register("command", ...args => {
         if (isNaN(page)) page = 1;
         if (page == 1) {
             ChatLib.chat(`&6-----------------------------------------------------`);
-            ChatLib.chat(ChatLib.getCenteredText("&6Hysentials Commands (1/1)"))
-            ChatLib.chat(ChatLib.getCenteredText('&7Basic Hysentials Commands'))
+            ChatLib.chat(cen1)
+            ChatLib.chat(cen2)
             ChatLib.chat('')
             ChatLib.chat('&6/hysentials help <page> &fView all the Hysentials commands.')
             ChatLib.chat('&6/hysentials config &fOpen the Hysentials config.')
